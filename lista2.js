a9b3f684c34eba6d6acc77d326bdb52e90aeeff6
miguelasaph <miguel.galli@escola.pr.gov.br>
2026-03-05 14:13:51 +0000

    lista

--- lista2.js
+++ lista2.js
@@ -1,7 +1,37 @@
-//criar um programa que mostre se o numero e positivo ou negativo 
+//Criar um programa que mostre se um número é positivo ou negativo.
 
-let num1 = 99
-if (num1>=0){
-    console.log(`o numero ${num1} e positivo`)
-    
+let num = 20;
+if (num > 0) {
+    console.log(`O número ${num} é positivo`)
+} else {
+    console.log(`O número ${num} é positivo`)
 }
+
+
+//Criar um programa que mostre se um ano é bissexto.
+
+let ano = 2002;
+if (ano % 4 === 0) {
+    console.log('É bissexto.')
+} else {
+    console.log('Não é bissexto.')
+}
+
+
+//Criar um programa que mostre uma senha e a mensagem Acesso permitido.
+
+let senha = 'amoeba123'
+if (senha === 'amoeba123') {
+    console.log('Acesso permitido.')
+} else {
+    console.log('Acesso negado.')
+}
+if (senha === 'amoeba123') {
+    console.log('Acesso permitido.')
+} else {
+    console.log('Acesso negado.'     )
+}
+//Criar um programa que mostre um desconto de 5% para compras maiores de R$100.
+//Criar um programa que que classifique uma pessoa como criança, adolescente ou adulto de acordo com a idade.
+//Criar um programa que mostre qual tipo de triângulo de acordo com seus lados (equilatero, isóceles e escaleno).
+//Criar um programa que mostre conceitos A B C ou D e acordo com as notas: >9.5 A; Entre 9.4 e 8.5 B; Entre 8.4 e 7.0 C; <6.9 D.
